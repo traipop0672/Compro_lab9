a14464a50d34bb7dab2e870fb90545cbb8bbaa3b
Traipop <traipopwichiansarn@gmail.com>
2025-12-19 15:01:10 +0700

    lab9_2 completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int x,int y){
+	if((x > 0)&&(y > 0)){
+		for(int i=0 ; i < x ; i++){
+			for(int j = 0; j < y ;j++){
+				cout << "O";
+			}
+		cout << "\n";
+		}
+	} else {
+	cout << "Invalid input";
+	}
+}	
 
 int main(){
 	
